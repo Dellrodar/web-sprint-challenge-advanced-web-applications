c3eb86cabee8210fe95ef1a08c569eddb548c2b4
Dellrodar <3357168+Dellrodar@users.noreply.github.com>
2020-07-19 19:51:51 -0400

    Login Complete * Login Page completed and working. * Gets Correct API token * PrivateRoute working.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
+import BubblePage from './components/BubblePage';
+import PrivateRoute from './utils/PrivateRoute';
 
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
+        <PrivateRoute exact path="/colors" component={BubblePage} />
+        {/*
+          Build a PrivateRoute component that will
+          display BubblePage when you're authenticated
         */}
       </div>
-    </Router>
   );
 }
 
